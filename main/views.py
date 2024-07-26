de3016609ef55bcd43c57c0da02cb1beca6486b2
GabrielVrabie007 <vrabiegabriel57@gmail.com>
2024-07-26 18:24:43 +0300

    Updating main app,pages index and about.

--- main/views.py
+++ main/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {"title": "Home", "content": "Main Page "}
+    context = {"title": "Home", "content": "Магазин мебели HOME"}
+
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About Us")
+    context = {
+        "title": "About",
+        "content": "More Information",
+        "text_on_page": "Goods for good price!",
+    }
+    return render(request, "main/about.html", context)

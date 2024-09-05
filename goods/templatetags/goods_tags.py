883f013a391ea25a4a683de1350e989d1852ec24
GabrielVrabie007 <vrabiegabriel57@gmail.com>
2024-09-05 23:20:23 +0300

    Product filter and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 from atexit import register
+from urllib.parse import urlencode
 from goods.models import Categories
 from django import template
 
@@ -8,3 +9,10 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context["request"].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -10,12 +10,20 @@ goods = Products.objects.all()
 def catalog(request, category_slug):
 
     page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
 
     if category_slug == "all":
         goods = Products.objects.all()
     else:
         # get_object_or_404 trebuie de folosit in randul de mai jos dar nu merge ceva
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
 

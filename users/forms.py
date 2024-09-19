c1bcb71d6bf8d9f1a6446ecd22130e79aecd04c4
GabrielVrabie007 <vrabiegabriel57@gmail.com>
2024-09-19 19:31:28 +0300

    Registration form and controller are ready.

--- users/forms.py
+++ users/forms.py
@@ -28,3 +28,24 @@ class UserLoginForm(AuthenticationForm):
     #     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
     #                                       'class': 'form-control',
     #                                       'placeholder': 'Введите ваш пароль'})
+
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
